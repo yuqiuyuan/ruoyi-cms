168461b3fd13cf5887a3d005e1b666b8d182a155
虞秋元 <yuqiuyuan@gokuaidian.com>
2020-04-15 00:14:50 +0800

    素材图片上传及展示

--- ruoyi-cms/src/main/java/com/ruoyi/cms/controller/CmsController.java
+++ ruoyi-cms/src/main/java/com/ruoyi/cms/controller/CmsController.java
@@ -1,10 +1,11 @@
 package com.ruoyi.cms.controller;
 
+import com.aliyun.oss.OSS;
+import com.aliyun.oss.OSSClientBuilder;
 import com.ruoyi.cms.domain.Attachment;
 import com.ruoyi.cms.domain.Pv;
 import com.ruoyi.cms.service.CmsService;
 import com.ruoyi.cms.service.IAttachmentService;
-import com.ruoyi.cms.service.IPvService;
 import com.ruoyi.cms.service.PVQueueService;
 import com.ruoyi.common.annotation.Log;
 import com.ruoyi.common.config.Global;
@@ -18,68 +19,91 @@ import com.ruoyi.common.utils.file.FileUploadUtils;
 import com.ruoyi.common.utils.file.MimeTypeUtils;
 import com.ruoyi.framework.util.ShiroUtils;
 import com.ruoyi.system.domain.SysUser;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 /**
  * cms模块的通用操作
+ *
+ * @author drebander
+ * @since 2020-04-14
  */
+@Slf4j
 @Controller
 public class CmsController extends BaseController {
 
-    @Autowired
+    @Resource
     private ServerConfig serverConfig;
-    @Autowired
+    @Resource
     private IAttachmentService attachmentService;
-
-    @Autowired
-    private IPvService pvService;
-    @Autowired
+    @Resource
+    private PVQueueService pvQueueService;
+    @Resource
     CmsService cms;
+    @Value("${cms.oss.endpoint}")
+    private String endpoint;
+
+
+    @Value("${cms.oss.accessKeyId}")
+    private String accessKeyId;
+
+    @Value("${cms.oss.accessKeySecret}")
+    private String accessKeySecret;
+
+    @Value("${cms.oss.bucketName}")
+    private String bucketName;
+
+    @Value("${cms.oss.objectName}")
+    private String objectName;
+
     /**
      * 上传附件请求
      */
     @PostMapping("/cms/uploadAttach")
     @ResponseBody
-    public AjaxResult uploadAttach(@RequestParam("zid") String zid,String sort, MultipartFile file) throws Exception
-    {
-        try
-        {
-            if(StringUtils.isEmpty(zid)){
+    public AjaxResult uploadAttach(@RequestParam("zid") String zid, String sort, MultipartFile file) {
+        try {
+            if (StringUtils.isEmpty(zid)) {
                 return AjaxResult.error("参数[zid]不能为空!");
             }
-            Attachment attachment=new Attachment();
-            if(StringUtils.isEmpty(sort)){
-                sort="1";
+            Attachment attachment = new Attachment();
+            if (StringUtils.isEmpty(sort)) {
+                sort = "1";
             }
-            int sortNum=1;
-            try{
-                sortNum=Integer.valueOf(sort);
-            }catch (Exception ex){}
+            int sortNum;
+            try {
+                sortNum = Integer.valueOf(sort);
+            } catch (Exception ex) {
+                return AjaxResult.error(ex.getMessage());
+            }
             attachment.setSort(sortNum);
-            String suffix=FileUploadUtils.getExtension(file);
-            String fileType= MimeTypeUtils.MATERIAL_TYPE_OTHER;//文件类型-其它
-            if(FileUploadUtils.isImage(suffix)){
-                fileType=MimeTypeUtils.MATERIAL_TYPE_IMG;
-            }else if(FileUploadUtils.isText(suffix)){
-                fileType=MimeTypeUtils.MATERIAL_TYPE_TEXT;
-            }else if(FileUploadUtils.isAudio(suffix)){
-                fileType=MimeTypeUtils.MATERIAL_TYPE_AUDIO;
-            }else if(FileUploadUtils.isVideo(suffix)){
-                fileType=MimeTypeUtils.MATERIAL_TYPE_VIDEO;
-            }else if(FileUploadUtils.isZip(suffix)){
-                fileType=MimeTypeUtils.MATERIAL_TYPE_ZIP;
-            }else{
-                fileType= MimeTypeUtils.MATERIAL_TYPE_OTHER;
+            String suffix = FileUploadUtils.getExtension(file);
+            String fileType;//文件类型-其它
+            if (FileUploadUtils.isImage(suffix)) {
+                fileType = MimeTypeUtils.MATERIAL_TYPE_IMG;
+            } else if (FileUploadUtils.isText(suffix)) {
+                fileType = MimeTypeUtils.MATERIAL_TYPE_TEXT;
+            } else if (FileUploadUtils.isAudio(suffix)) {
+                fileType = MimeTypeUtils.MATERIAL_TYPE_AUDIO;
+            } else if (FileUploadUtils.isVideo(suffix)) {
+                fileType = MimeTypeUtils.MATERIAL_TYPE_VIDEO;
+            } else if (FileUploadUtils.isZip(suffix)) {
+                fileType = MimeTypeUtils.MATERIAL_TYPE_ZIP;
+            } else {
+                fileType = MimeTypeUtils.MATERIAL_TYPE_OTHER;
             }
             // 上传并返回新文件名称
             String path = FileUploadUtils.upload(Global.getAttachPath(), file);
@@ -93,9 +117,7 @@ public class CmsController extends BaseController {
 
             attachmentService.insertAttachment(attachment);
             return AjaxResult.success(attachment);
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             return AjaxResult.error(e.getMessage());
         }
     }
@@ -105,8 +127,7 @@ public class CmsController extends BaseController {
      */
     @PostMapping("/cms/selectAttachmentsByZid")
     @ResponseBody
-    public Object selectAttachmentsByZid(@RequestParam("zid")String  zid)
-    {
+    public Object selectAttachmentsByZid(@RequestParam("zid") String zid) {
         List<Attachment> list = attachmentService.selectAttachmentsByZid(zid);
         return AjaxResult.success(list);
     }
@@ -115,28 +136,27 @@ public class CmsController extends BaseController {
      * 删除附件
      */
     @Log(title = "附件", businessType = BusinessType.DELETE)
-    @PostMapping( "/cms/deleteAttachments")
+    @PostMapping("/cms/deleteAttachments")
     @ResponseBody
-    public AjaxResult remove(String ids)
-    {
-        SysUser user= ShiroUtils.getSysUser();
-        boolean checkOkFlag=true;
-        if(!user.isAdmin()){
+    public AjaxResult remove(String ids) {
+        SysUser user = ShiroUtils.getSysUser();
+        boolean checkOkFlag = true;
+        if (!user.isAdmin()) {
             //非管理员用户调用该接口删除只能删除自己的附件
-            String[] arr= Convert.toStrArray(ids);
-            for(String id:arr){
-                if(StringUtils.isNotEmpty(id)){
-                    Attachment attachment=attachmentService.selectAttachmentById(id);
-                    if(attachment!=null){
-                        if(!attachment.getUserId().equals(user.getUserId().toString())){
-                            checkOkFlag=false;
+            String[] arr = Convert.toStrArray(ids);
+            for (String id : arr) {
+                if (StringUtils.isNotEmpty(id)) {
+                    Attachment attachment = attachmentService.selectAttachmentById(id);
+                    if (attachment != null) {
+                        if (!attachment.getUserId().equals(user.getUserId().toString())) {
+                            checkOkFlag = false;
                             break;
                         }
                     }
                 }
             }
         }
-        if(!checkOkFlag){
+        if (!checkOkFlag) {
             return AjaxResult.error("您只能删除自己上传的附件!");
         }
         return toAjax(attachmentService.deleteAttachmentByIds(ids));
@@ -148,46 +168,47 @@ public class CmsController extends BaseController {
      */
     @PostMapping("/cms/uploadMaterial")
     @ResponseBody
-    public AjaxResult uploadMaterial(MultipartFile file) throws Exception
-    {
-        try
-        {
-            // 上传并返回新文件名称
-            String path = FileUploadUtils.upload(Global.getMaterialPath(), file);
-            String url = serverConfig.getUrl() + path;
-            Map<String,Object> data=new HashMap();
-            int width=FileUploadUtils.getImgWidth(file);
-            int height=FileUploadUtils.getImgHeight(file);
-            data.put("path",path);
-            data.put("url",url);
-            data.put("width",width);
-            data.put("suffix",FileUploadUtils.getExtension(file));
-            data.put("height",height);
-            data.put("size",file.getSize());
-            data.put("name",file.getOriginalFilename());
+    public AjaxResult uploadMaterial(MultipartFile file) {
+
+        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
+        // 上传Byte数组。
+        byte[] content;
+        try {
+            String object = objectName + File.separator + "material" + File.separator + file.getOriginalFilename();
+            content = file.getBytes();
+            ossClient.putObject(bucketName, object, new ByteArrayInputStream(content));
+            // 关闭OSSClient。
+            final String url = String.format("https://%s.%s/%s", bucketName, endpoint, object);
+            HashMap<String, Object> data = new HashMap<>(7);
+            int width = FileUploadUtils.getImgWidth(file);
+            int height = FileUploadUtils.getImgHeight(file);
+            data.put("path", url);
+            data.put("url", url);
+            data.put("width", width);
+            data.put("suffix", FileUploadUtils.getExtension(file));
+            data.put("height", height);
+            data.put("size", file.getSize());
+            data.put("name", file.getOriginalFilename());
             return AjaxResult.success(data);
-        }
-        catch (Exception e)
-        {
+        } catch (IOException e) {
+            log.error("OSS upload file occur IO exception ~!", e);
             return AjaxResult.error(e.getMessage());
+        } finally {
+            ossClient.shutdown();
         }
     }
 
-    @Autowired
-    PVQueueService pvQueueService;
 
-    @PostMapping( "/page/view")
+    @PostMapping("/page/view")
     @ResponseBody
-    public AjaxResult pv(Pv pv, HttpServletRequest request)
-    {
-        pvQueueService.pushPvQueue(request,pv);
+    public AjaxResult pv(Pv pv, HttpServletRequest request) {
+        pvQueueService.pushPvQueue(request, pv);
         return success();
     }
 
-    @PostMapping( "/cms/clearSiteInfoCache")
+    @PostMapping("/cms/clearSiteInfoCache")
     @ResponseBody
-    public AjaxResult clearSiteInfoCache(Pv pv, HttpServletRequest request)
-    {
+    public AjaxResult clearSiteInfoCache() {
         cms.clearCache();
         return success();
     }
